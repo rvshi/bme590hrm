124a1e14476b98503d3f48541d3967f2a10228c1
Harvey Shi <harveyyshi@gmail.com>
2018-02-23 18:55:00 -0500

    Adds test cases for file input. - Also fixes PEP8 issues with main class

--- test_HRMonitor.py
+++ test_HRMonitor.py
@@ -1,6 +1,5 @@
 import pytest
 
-
 test_files_path = 'test_data/'
 
 
@@ -10,8 +9,28 @@ def get_test_file(index):
 
 
 def test_init():
+    """Basic test to see if calling the constructor works
+    """
     from hrmonitor import HRMonitor
     HRMonitor(get_test_file(1))
+
+
+def test_file_not_found():
+    """Test for checking how the class handles files that do not exist
+    """
+    with pytest.raises(FileNotFoundError):
+        from hrmonitor import HRMonitor
+        HRMonitor('./file-that-does-not-exist.csv')
+
+
+def test_filetype():
+    """Test for checking if the class correctly identifies unsupported file types
+    """
+    from hrmonitor import HRMonitor
+    unsupported_filetypes = ['', '.', '.json', '.not-a-real-file-type']
+    for f in unsupported_filetypes:
+        with pytest.raises(ValueError):
+            HRMonitor('fake-file' + f)
 
 
 if __name__ == '__main__':
